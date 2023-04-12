d389bda77149bbf98bcc95d3610cebb7f22dd87b
Gabriel Vasconcelos <gabrielvasco3431@gmail.com>
2023-04-11 22:19:35 -0300

    finalizado

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def resultados():
     elif request.method == 'POST':
         mes_selecionado = request.form['mes']
 
-        #print('mes_selecionado', mes_selecionado)
+        ##print('mes_selecionado', mes_selecionado)
 
         with open('data.json', 'r') as json_file:
             data_list = json.load(json_file)
@@ -39,19 +39,15 @@ def resultados():
             
             data_str = d["data"]
             data = datetime.strptime(data_str, "%Y-%m-%d")
-            #print("data_str: ", data_str)
-            #print("data: ", data)
-            #print('data.month: ', data.month)
-            #print('mes_selecionado: ', mes_selecionado)
+            ##print("data_str: ", data_str)
+            ##print("data: ", data)
+            ##print('data.month: ', data.month)
+            ##print('mes_selecionado: ', mes_selecionado)
             if str(data.month) == mes_selecionado:
-                print('sim')
+                #print('sim')
                 filtered_data.append(d)
-            else:
-                print('nao')
 
-        print('filtered_data: ', filtered_data)
-
-        
+        #print('filtered_data: ', filtered_data)
 
 
         # Convertendo o objeto Python em um DataFrame do pandas
@@ -65,15 +61,14 @@ def resultados():
             df_acumulado = []
 
         # Exibindo o DataFrame resultante
-        print(df_acumulado)
+        #print(df_acumulado)
         try:
-            colors = ['gold', 'mediumturquoise', 'darkorange', 'lightgreen']
             labels = df_acumulado['nome'].tolist()
             values = df_acumulado['volume'].tolist()
             values = [int(val) for val in df_acumulado['volume']]
 
 
-            fig = go.Figure(data=[go.Bar(x=labels, y=values)])
+            fig = go.Figure(data=[go.Bar(x=labels, y=values, marker=dict(color='#ff6543'))])
             fig.update_layout(title=f'Gráfico de consumo de litros por pessoa referente ao mês de {meses[int(mes_selecionado)-1]}',
                             xaxis_title='Nome',
                             yaxis_title='Volume')
@@ -85,7 +80,33 @@ def resultados():
         except:
             graphJSON = ''
 
-        return render_template('resultados.html', filtered_data=filtered_data, graphJSON=graphJSON)
+        
+        try:
+            df2 = pd.DataFrame(filtered_data)
+            df2['volume'] = df2['volume'].astype(int)
+
+            # Converter a coluna de data para o tipo datetime
+            df2['data'] = pd.to_datetime(df2['data'])
+
+            # Agrupar os volumes por data
+            df_agrupado2 = df2.groupby('data')['volume'].sum().reset_index()
+
+            # Criar um objeto trace para o gráfico de linhas
+            trace = go.Scatter(x=df_agrupado2['data'], y=df_agrupado2['volume'], mode='lines', line=dict(color='#ff6543'))
+
+            # Criar layout para o gráfico
+            layout2 = go.Layout(title=f'Volume por Dia referente ao mês de {meses[int(mes_selecionado)-1]}', xaxis=dict(title='Data'), yaxis=dict(title='Volume'))
+
+            # Criar objeto figura que contém trace e layout e plota o gráfico
+            fig2 = go.Figure(data=[trace], layout=layout2)
+
+            graphJSON2 = pio.to_json(fig2)
+        
+        except:
+            graphJSON2 = ''
+
+
+        return render_template('resultados.html', filtered_data=filtered_data, graphJSON=graphJSON, graphJSON2=graphJSON2)
 
 
 @app.route('/enviar', methods=['POST'])
@@ -95,7 +116,7 @@ def enviar():
     data['data'] = request.form['data']
     data['volume'] = request.form['volume']
 
-    print(data)
+    #print(data)
 
     try:
         with open('data.json', 'r') as json_file:
